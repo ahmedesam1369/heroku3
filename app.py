dc79b84777e49eb1cf443208db094bc140c254a9
Ahmed Esam <ahmedesam1369@gmail.com>
2022-07-02 23:39:57 +0200

    ff

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello'
+
+@app.route('/api', methods = ['GET'])
+def returnascii():
+    d = {}
+    inputchar = str(request.args['query'])
+    # inputchar = 'a'
+    answer = str(ord(inputchar))
+    d['output'] = answer
+    
+    return d
+
+
+
+if __name__ == "__main__":
+    app.run()
+
+
